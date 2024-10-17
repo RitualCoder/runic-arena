442cbec81aba2fc899f07148404fbfe922a3c97e
Enzo Cornaggia <101344725+RitualCoder@users.noreply.github.com>
2024-10-17 12:36:16 +0200

    feat: authentication

---
+++ src/Enum/CardType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enum;
+
+enum CardType: string
+{
+    case FIRE = 'fire';
+    case WATER = 'water';
+    case GRASS = 'grass';
+    case NORMAL = 'normal';
+    case ROCK = 'rock';
+    case ELECTRIC = 'electric';
+    case FIGHTING = 'fighting';
+    case DARK = 'dark';
+    case STEEL = 'steel';
+    case FAIRY = 'fairy';
+    case DRAGON = 'dragon';
+    case POISON = 'poison';
+}
